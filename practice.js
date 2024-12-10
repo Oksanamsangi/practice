56be278d906b0155f1afbb8d75dcb876ac933e05
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2024-12-10 02:05:58 -0600

    practice

--- practice.js
+++ practice.js
@@ -468,3 +468,179 @@
 // console.log(getTotalBalanceByGender(clients, "male")); // 12053
 
 // console.log(getTotalBalanceByGender(clients, "female")); // 8863
+
+// const changeColor = function (color) {
+//   this.color = color;
+// };
+// const hat = {
+//   color: "balack",
+// };
+// // changeColor.call(hat, "yellow");
+// // console.log(hat);
+
+// const changeHatColor = changeColor.bind(hat, "yellow");
+// changeHatColor();
+// console.log(hat);
+
+// const user = {
+//   name: "Luis",
+//   age: 30,
+
+//   showThis() {
+//     console.log("This--->", this);
+//   },
+
+//   showName() {
+//     console.log(this.name);
+//   },
+// };
+
+// const someFunction = (callback) => {
+//   callback();
+// };
+
+// someFunction(user.showName.bind(user));
+
+// class User {
+//   #password;
+//   constructor(firstName, lastName, login, password) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.login = login;
+//     this.password = password;
+//   }
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+
+//   get password() {
+//     return this.#password;
+//   }
+// }
+
+// const user = new User("Oksana", "Sydorenko", "koksyk", "jfdj9000");
+// console.log(user);
+// // console.log(user.getFullName());
+// // console.log(user.#password);
+// // user.setPassword("newpassword");
+// // console.log(user.getPassword);
+// console.log(user.#password);
+
+// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі.
+// Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+
+// Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів.
+// У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
+
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// ?Створи клас Storage, який створюватиме об'єкти для управління складом товарів.
+// Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
+
+// Оголоси наступні методи класу:
+
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів
+// у приватній властивості items об'єкта.
+
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки
+// коректності роботи.У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
+
+// class Storage {
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+//   getItems() {
+//     return this.#items;
+//   }
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter((el) => {
+//       return el !== itemToRemove;
+//     });
+//   }
+// }
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// storage.removeItem("Scaner");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// ?Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у
+// приватну властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+
+// class StringBuilder {
+//   #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+//   getValue() {
+//     return this.#value;
+//   }
+//   padEnd(str) {
+//     this.#value = this.#value + str;
+//   }
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
